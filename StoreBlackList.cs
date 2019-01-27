419e378bf1f68c20b44e150d873f45caad966cfe
ElijahThor <38489422+ElijahThor@users.noreply.github.com>
2019-01-27 13:18:06 +0800

    Add files via upload

---
+++ StoreBlackList.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawBotFYP_GUI_.Core.Data
+{
+    public class StoreBlackList
+    {
+        public List<string> blackList = new List<string>(); // Stores Vulgarities
+
+        public StoreBlackList()
+        {
+            var relpath = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location).Replace(@"bin\Debug", @"Core\Data\BlackList.txt");
+            string[] blacklist = System.IO.File.ReadAllLines(relpath.ToString()); //Directory for blacklist.txt
+
+            try
+            {
+                foreach (string line in blacklist)
+                {
+                    blackList.Add(line);
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+    }
+}
+
